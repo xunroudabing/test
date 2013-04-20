9d15d5db8db161b9976639f8591a936f5498c5fd
hanzheng <hzroom110@163.com>
2013-04-20 05:50:19 -0430

    Change-Id: I5f2d51fbfddcb9da7c31f90ae7c13b27469a3c92 Signed-off-by: hanzheng <hzroom110@163.com>

--- BaiduSDKDemo/src/com/baidu/mobads/demo/main/BaiduSDKDemo.java
+++ BaiduSDKDemo/src/com/baidu/mobads/demo/main/BaiduSDKDemo.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+/**
+ * 大饼测试
+ * @author DaBing
+ *
+ */
 public class BaiduSDKDemo extends Activity {
 	Button advanced;
 	Button simpleDeclaring;
